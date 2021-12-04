ed1e320fef4a1cf16939ed7509682bf6fd0f8228
soph0002 <uldrylavergne.s@husky.neu.edu>
2021-12-03 23:14:42 -0500

    updated general theme

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,20 +6,22 @@ const Home = () => {
     return(
         <>
         <h1>Shop</h1>
-        <ul>
-            <li>
-                <Link>Category 1</Link>
-            </li>
-            <li>
-                <Link>Category 2</Link>
-            </li>
-            <li>
-                <Link>Category 3</Link>
-            </li>
-            <li>
-                <Link>Category 4</Link>
-            </li>
-        </ul>
+            <div className="col-2">
+                <ul className="list-group">
+                    <li className="list-group-item background-color-light override-bs">
+                        <Link>Category 1</Link>
+                    </li>
+                    <li className="list-group-item background-color-light override-bs">
+                        <Link>Category 2</Link>
+                    </li>
+                    <li className="list-group-item background-color-light override-bs">
+                        <Link>Category 3</Link>
+                    </li>
+                    <li className="list-group-item background-color-light override-bs">
+                        <Link>Category 4</Link>
+                    </li>
+                </ul>
+            </div>
         <h1>Recently Viewed</h1>
         </>
     )
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,26 +7,48 @@ import SearchBar from "./SearchBar";
 
 const Navigation = () => {
     return (
-        <div className="container mt-2">
-            <div className="row">
-                <div className="col-2">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid mt-2">
+                <div className="col-1 ms-3 mb-3">
                     <Link to="/home">
-                        <i className="fas fa-home"></i>
+                        <i className="fas fa-home fa-3x"/>
                     </Link>
                 </div>
-                <div className="col-6">
-                    <SearchBar />
-                </div>
-                <div className="col-4 list-group list-group-horizontal">
+                <button className="col-1 navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"/>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarColor03">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item mb-2">Home<span
+                            className="visually-hidden">(current)</span></li>
+                        <li className="nav-item mb-2">Details</li>
+                        {/* ^^ placeholder for potential future implementation,,, do the $active
+                         check for this if used*/}
+                    </ul>
+
+                        <div className="d-flex">
+                            <SearchBar/>
+                        </div>
+                <div className="col-2 mb-3 btn-group" role="group">
                     <Link to="/profile">
-                        <button className="btn btn-primary list-group-item mx-2">My Profile</button>
+                        <button type="button" className="btn btn-primary button-group-border my-2 my-sm-0 my-md-0 ms-2 override-bs">Profile</button>
                     </Link>
-                    <Link to="/login">
-                        <button className="btn btn-primary list-group-item mx-2">Login</button>
-                    </Link>
+                    <div className="btn-group" role="group">
+                        <button  id="btnGroupDrop1" type="button"
+                                 className="btn btn-primary dropdown-toggle"
+                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"/>
+                        <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                            <a className="dropdown-item" href="#">Dropdown link</a>
+                            <Link to="/login">
+                                <span className="dropdown-item">Login</span>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
-        </div>
+        </nav>
     );
 }
 export default Navigation;
