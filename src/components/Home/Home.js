c742ecd06b352c2267616a1b56f94c543d3476e4
soph0002 <uldrylavergne.s@husky.neu.edu>
2021-12-07 17:29:20 -0500

    home page basic formatting complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,24 +2,30 @@ import React from "react";
 import "../../vendors/bootstrap/bootstrap.min.css"
 import {Link} from "react-router-dom"
 
-const Home = () => {
+const Home = ({
+active = 'cat1'
+              }) => {
     return(
         <>
         <h1>Shop</h1>
-            <div className="col-2">
-                <ul className="list-group">
-                    <li className="list-group-item background-color-light override-bs">
-                        <Link>Category 1</Link>
-                    </li>
-                    <li className="list-group-item background-color-light override-bs">
-                        <Link>Category 2</Link>
-                    </li>
-                    <li className="list-group-item background-color-light override-bs">
-                        <Link>Category 3</Link>
-                    </li>
-                    <li className="list-group-item background-color-light override-bs">
-                        <Link>Category 4</Link>
-                    </li>
+            <div className="col-2 ms-3">
+                <ul className={`list-group`}>
+                    <Link to="/shop/home" className={`list-group-item remove-decorations override-bs ${active === 'cat1' ? 'active' : ''}`}>
+                        <i className={"fas fa-home me-3"}/>
+                        <span className={"d-sm-none d-xl-inline"}>Category 1</span>
+                    </Link>
+                    <Link to="/shop/explore" className={`list-group-item remove-decorations override-bs ${active === 'cat2' ? 'active' : ''}`}>
+                        <i className={"fas fa-hashtag me-3"}/>
+                        <span className={"d-sm-none d-xl-inline"}>Category 2</span>
+                    </Link>
+                    <Link to="/shop/notifications" className={`list-group-item remove-decorations override-bs ${active === 'cat3' ? 'active' : ''}`}>
+                        <i className={"fas fa-bell me-3"}/>
+                        <span className={"d-sm-none d-xl-inline"}>Category 3</span>
+                    </Link>
+                    <Link to="/shop/messages" className={`list-group-item remove-decorations override-bs ${active === 'cat4' ? 'active' : ''}`}>
+                        <i className={"fas fa-envelope me-3"}/>
+                        <span className={"d-sm-none d-xl-inline"}>Category 4</span>
+                    </Link>
                 </ul>
             </div>
         <h1>Recently Viewed</h1>
