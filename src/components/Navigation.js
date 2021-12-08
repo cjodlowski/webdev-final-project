d4def4418dfc6721b1b7bc9ff2096b6fd7b0abd8
soph0002 <uldrylavergne.s@husky.neu.edu>
2021-12-07 19:58:18 -0500

    refactored

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,31 +8,31 @@ import SearchBar from "./SearchBar";
 const Navigation = (active = 'featured') => {
     return (
         <nav className="navbar navbar-expand navbar-dark bg-primary">
-            <div className="container-fluid mt-2 d-flex flex-row">
-                <div className="d-flex justify-content-start">
+            <div className="container-fluid d-flex flex-row">
+                <div className="d-flex justify-content-start ms-3 me-3">
                     <Link to="/home">
                         <span className={"remove-decorations override-bs title-format" +
-                        " color-light"}>Shop</span>
+                        " text-light"}>Shop</span>
                     </Link>
                 </div>
-                <div className={"d-flex justify-content-center"}>
+                <div className={"d-flex justify-content-center mt-1"}>
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className={`nav-link ${active === 'featured' ? 'active' : ''}`} href="#">Featured</a>
+                            <Link className={`nav-link ${active === 'featured' ? 'active' : ''}`} to="#">Featured</Link>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link ${active === 'recents' ? 'active' : ''}`} href="#">Recents</a>
+                            <Link className={`nav-link ${active === 'recents' ? 'active' : ''}`} to="#">Recents</Link>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link ${active === 'policies' ? 'active' : ''}`} href="#">Policies</a>
+                            <Link className={`nav-link ${active === 'policies' ? 'active' : ''}`} to="/privacypolicy">Policies</Link>
                         </li>
                     </ul>
                 </div>
                 <div className={"d-flex justify-content-end"}>
-                    <div className={"col-3 col-md-4 col-lg-8 me-1"}>
+                    <div className={"col-3 col-md-4 col-lg-8 me-1 mt-3"}>
                         <SearchBar/>
                     </div>
-                    <div className="col-4 mb-3 btn-group" role="group">
+                    <div className="col-4 mb-3 mt-3 btn-group" role="group">
                             <Link to="/profile">
                                 <button type="button" className="btn btn-light button-group-border override-bs">Profile</button>
                             </Link>
