41bed1380c239a0164dd875a8843761d2f48340e
cjodlowski <cnjodlowski@gmail.com>
2021-12-13 18:20:46 -0500

    bookmark list

--- src/components/Profile/BookmarkList.js
+++ src/components/Profile/BookmarkList.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Bookmark from "./Bookmark"
 import CatalogItem from "../Home/Catalog/CatalogItem";
 
+import {findAllItems} from "../../services/item-service"
+
 const BookmarkList = () => {
+    const [items, setItems] = useState([]);
+    useEffect(() => {findAllItems().then(received => setItems(received))});
+
     return(
         <div className="mx-2">
             <h2>Bookmarks</h2>
             <ul className="list-group">
-                {/* map bookmark here */}
-                <CatalogItem/>
+                {
+                    items.map((item, key) => {
+                        console.log(item);
+                        return <CatalogItem key={key} item={item} />
+                    })
+                }
             </ul>
         </div>
     )
