52b9672277de676bc9b027e4f95c017160573587
cjodlowski <cnjodlowski@gmail.com>
2021-12-14 12:07:44 -0500

    can see user bookmarks now

--- src/components/Profile/BookmarkList.js
+++ src/components/Profile/BookmarkList.js
@@ -1,30 +1,23 @@
 import React, { useEffect, useState } from "react";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom"
 import CatalogItem from "../Home/Catalog/CatalogItem";
 import { findUserbyId } from "../../services/user-service";
-import { findItemById } from "../../services/item-service";
+import { findFiltered } from "../../services/item-service";
 
 
 const BookmarkList = () => {
     //const [items, setItems] = useState([]);
     const [user, setUser] = useState({});
-    const [currbm, setBookmark] = useState({});
+    const [bookmarks, setBookmark] = useState([]);
     const params = useParams();
-    useEffect(() => {findUserbyId(params.id).then(result => setUser(result))});
-    // TODO: setBookmark changes the state of the bookmark, which means all items are
-    // rendered as the same item, and are rapidly switching back and forth
-    // Need a good way to get an array of bookmark OBJECTS and not IDS, so that we can map through that instead
-    const getBookmark = (id) => {findItemById(id).then(result => setBookmark(result))};
-    if(user.bookmarks == undefined) {
-        user.bookmarks = [];
-    }
-    return(
+    useEffect(() => { findUserbyId(params.id).then(result => setUser(result)) });
+    useEffect(() => findFiltered(user.bookmarks).then(result => setBookmark(result)));
+    return (
         <div className="mx-2 mt-3 container">
             <div className={"row ms-3"}>
                 {
-                    user.bookmarks.map((item, key) => {
-                        getBookmark(item);
-                        return <CatalogItem key={key} item={currbm} />
+                    bookmarks.map((item, key) => {
+                        return <CatalogItem key={key} item={item} />
                     })
                 }
             </div>
--- src/services/item-service.js
+++ src/services/item-service.js
@@ -11,11 +11,22 @@ export const findFeatured = () => {
 }
 
 export const findSearchResults = (tags) => {
-    return fetch(`http://localhost:4000/api/items/search/${tags}`)
+    return fetch(`http://localhost:4000/api/items/search/id/${tags}`)
     .then(response => response.json());
 }
 
 export const findItemById = (id) => {
     return fetch(`http://localhost:4000/api/items/${id}`)
     .then(response => response.json());
+}
+
+export const findFiltered = (idarr) => {
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(idarr),
+        headers: {
+            'content-type': 'application/json'
+        }
+    };
+    return fetch(`http://localhost:4000/api/items/filtered`, requestOptions).then(response => response.json());
 }
